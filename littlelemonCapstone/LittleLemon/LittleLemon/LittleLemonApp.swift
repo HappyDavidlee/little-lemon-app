e5feec68eb10405367a95a65a8fad41ce35afc4f
David.li <1441836644@qq.com>
2023-05-03 09:56:10 +0800

    Add files via upload
    Add files to git repo from local

---
+++ littlelemonCapstone/LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemonApp
+//
+//  Created by Aaraiz on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
